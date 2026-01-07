992d1fedb07ce1037e6dbe9fb7c57ae9c5ff0bb8
cxnkk <cenkoezkartal@gmail.com>
2026-01-07 12:52:04 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,45 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
 use std::process::exit;
+use std::{env, fs};
+
+enum Command {
+    Exit,
+    Echo,
+    Type,
+    CommandNotFound,
+}
+
+impl Command {
+    fn parse(s: &str) -> Self {
+        match s {
+            "exit" => Command::Exit,
+            "echo" => Command::Echo,
+            "type" => Command::Type,
+            _ => Command::CommandNotFound,
+        }
+    }
+}
+
+fn find_executable(command_name: &str) -> Option<String> {
+    if let Ok(path_var) = env::var("PATH") {
+        for path in path_var.split(":") {
+            let full_path = format!("{}/{}", path, command_name);
+            if Path::new(&full_path).exists() {
+                if let Ok(metadata) = fs::metadata(&full_path) {
+                    let mode = metadata.permissions().mode();
+
+                    if mode & 0o111 != 0 {
+                        return Some(full_path);
+                    }
+                }
+            }
+        }
+    }
+    None
+}
 
 fn main() {
     loop {
@@ -9,19 +48,27 @@ fn main() {
 
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
-        let trimmed_cmd = command.trim();
-        let input: Vec<&str> = trimmed_cmd.splitn(2, " ").collect();
-        match input[0] {
-            "exit" => exit(0),
-            "echo" => println!("{}", input[1]),
-            "type" => match input[1] {
-                "exit" => println!("exit is a shell builtin"),
-                "echo" => println!("echo is a shell builtin"),
-                "type" => println!("type is a shell builtin"),
-                _ => println!("{}: not found", input[1]),
+        let input = command.trim();
+        let parts: Vec<&str> = input.split_whitespace().collect();
+
+        match Command::parse(parts[0]) {
+            Command::Exit => exit(0),
+            Command::Echo => {
+                let msg = &parts[1..];
+                println!("{}", msg.join(" "))
+            }
+            Command::Type => match parts[1] {
+                "exit" | "echo" | "type" => println!("{} is a shell builtin", parts[1]),
+                _ => {
+                    if let Some(path) = find_executable(parts[1]) {
+                        println!("{} is {}", parts[1], path);
+                    } else {
+                        println!("{}: not found", parts[1])
+                    }
+                }
             },
-            _ => {
-                println!("{}: command not found", trimmed_cmd);
+            Command::CommandNotFound => {
+                println!("{}: command not found", input);
             }
         }
     }
